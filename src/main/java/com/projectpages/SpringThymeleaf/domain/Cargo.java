90c93828ee139eec8a2674c84ae460e208f37454
silviafds <silviafernandes974@gmail.com>
2023-03-20 14:53:48 -0300

    feat: classe endereco, funcionario e enum UF adicionada

--- src/main/java/com/projectpages/SpringThymeleaf/domain/Cargo.java
+++ src/main/java/com/projectpages/SpringThymeleaf/domain/Cargo.java
@@ -1,9 +1,12 @@
 package com.projectpages.SpringThymeleaf.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,6 +19,9 @@ public class Cargo extends AbstractEntity<Long> {
 	@ManyToOne
 	@JoinColumn(name = "id_departamento_fk")
 	private Departamento departamento;
+	
+	@OneToMany(mappedBy="cargo")
+	private List<Funcionario> funcionarios;
 
 	public String getNome() {
 		return nome;
@@ -31,6 +37,14 @@ public class Cargo extends AbstractEntity<Long> {
 
 	public void setDepartamento(Departamento departamento) {
 		this.departamento = departamento;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
 	}
 	
 	
